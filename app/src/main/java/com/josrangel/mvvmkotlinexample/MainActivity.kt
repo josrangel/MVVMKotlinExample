2f60282ad17ca12544dbf8018ada5be8940cc191
josrangel <josrangel@hotmail.com>
2022-01-22 18:41:35 -0600

    - add viewBinding - add observer for mvvm

--- app/src/main/java/com/josrangel/mvvmkotlinexample/MainActivity.kt
+++ app/src/main/java/com/josrangel/mvvmkotlinexample/MainActivity.kt
@@ -2,10 +2,35 @@ package com.josrangel.mvvmkotlinexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.josrangel.mvvmkotlinexample.databinding.ActivityMainBinding
+import com.josrangel.mvvmkotlinexample.viewmodel.QuoteViewModel
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val quoteViewModel: QuoteViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        addObserver()
+        initUI()
+    }
+
+    fun initUI() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        quoteViewModel.randomQuote()
+        binding.viewContainer.setOnClickListener { quoteViewModel.randomQuote() }
+    }
+
+    fun addObserver() {
+        quoteViewModel.quoteModel.observe(this, Observer { currentQuote ->
+            binding.tvQuote.text = currentQuote.quote
+            binding.tvAuthor.text = currentQuote.author
+        })
     }
 }
